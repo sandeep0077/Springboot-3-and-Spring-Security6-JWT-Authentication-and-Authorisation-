7d325887cbe9628664e2ea160a9b7d445d4ff700
sandeep0077 <sm671903@gmail.com>
2023-08-09 21:56:47 +0530

    Pausing this due to csrf removal in SecurityConfiguration class

--- src/main/java/com/springsecurity/security/config/SecurityConfiguration.java
+++ src/main/java/com/springsecurity/security/config/SecurityConfiguration.java
@@ -1,4 +1,42 @@
 package com.springsecurity.security.config;
 
+
+import jakarta.servlet.Filter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+    private final JwtConfigurationFilter jwtAuthenticatin;
+
+    private final AuthenticationProvider authenticationProvider;
+
+    //whitelisting
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .requestMatchers("")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthenticatin, UsernamePasswordAuthenticationFilter.class);
+    }
 }
